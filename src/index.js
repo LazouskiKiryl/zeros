249ce2d0b1ec412cd618889a15f1fa42aa1106a5
LazouskiKiryl <kiryllazouski@gmail.com>
2019-10-08 23:12:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let counter2 = 0;
+  let counter5 = 0;
+  
+  expression.split('*').forEach((value) => {
+    if (value.includes('!!')) {
+      countFactors(parseInt(value), 2);
+    } else {
+      countFactors(parseInt(value), 1);
+    }
+  });
+  
+  return counter2 > counter5 ? counter5 : counter2;
+  
+  function countFactors(number, step) {
+    for (let num = number; num > 1;  num -= step) {
+      counter2 += count(num, 2);
+      counter5 += count(num, 5);
+    }
+  }
+
+  function count(number, factor) {
+    let count = 0;
+    while (number % factor == 0) {
+      number /= factor;
+      count++;
+    }
+    return count;
+  }
+
 }
